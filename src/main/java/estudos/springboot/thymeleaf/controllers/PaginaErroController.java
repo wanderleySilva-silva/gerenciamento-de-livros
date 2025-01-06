e3297884f1e4552e0a6944ea9a6e274209431c82
wanderleySilva-silva <jwps.17@hotmail.com>
2025-01-06 09:59:48 -0300

    adicionada implementação de error controller

--- src/main/java/estudos/springboot/thymeleaf/controllers/PaginaErroController.java
+++ src/main/java/estudos/springboot/thymeleaf/controllers/PaginaErroController.java
@@ -1,10 +1,11 @@
 package estudos.springboot.thymeleaf.controllers;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class PaginaErroController {
+public class PaginaErroController implements ErrorController{
 	
 	@RequestMapping("/erro")
 	public String handleError() {
