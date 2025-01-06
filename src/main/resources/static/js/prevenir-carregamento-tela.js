a25afb303c8d34486c4f5662bde7d3720a423b3a
wanderleySilva-silva <jwps.17@hotmail.com>
2025-01-06 10:11:53 -0300

    retirado o alert do js

--- src/main/resources/static/js/prevenir-carregamento-tela.js
+++ src/main/resources/static/js/prevenir-carregamento-tela.js
@@ -1,7 +1,5 @@
 document.getElementById("botao-salvar").addEventListener("click", function(event){
 	event.preventDefault();
 	
-	alert("O botão foi clicado, mas o formulário não foi enviado!")
-	
 	document.getElementById("formLivro").submit();
 });
