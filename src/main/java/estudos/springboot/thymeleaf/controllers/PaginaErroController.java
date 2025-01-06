d62fb730ba414b9fa500a473800af7870a81b3d6
wanderleySilva-silva <jwps.17@hotmail.com>
2025-01-06 09:37:48 -0300

    adicionada novamente pánina de erro

---
+++ src/main/java/estudos/springboot/thymeleaf/controllers/PaginaErroController.java
@@ -0,0 +1,13 @@
+package estudos.springboot.thymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PaginaErroController {
+	@RequestMapping("/erro")
+	public String handleError() {
+
+		return "erro/erro";
+	}
+}
